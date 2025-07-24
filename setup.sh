890291c652911a27cfc5a1ef6380700be7cc4c43
kuldeep lodhi <144382128+kuldeeplodi@users.noreply.github.com>
2025-07-25 00:31:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Download model/data files from GitHub Releases
+echo "Downloading movie_dict.pkl and similarity.pkl..."
+
+wget -O movie_dict.pkl https://github.com/kuldeeplodi/movie_recommendation_system/releases/download/movie/movie_dict.pkl
+wget -O similarity.pkl https://github.com/kuldeeplodi/movie_recommendation_system/releases/download/movie/similarity.pkl
+echo "Download complete."
